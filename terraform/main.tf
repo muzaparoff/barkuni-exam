49b765b910217c40e26732cd8a5a9ae3de33309f
muzaparoff <muzyukins@gmail.com>
2025-05-20 20:39:37 +0300

    add: fixes cd

--- terraform/main.tf
+++ terraform/main.tf
@@ -104,19 +104,6 @@ data "aws_route53_zone" "main" {
   private_zone = false
 }
 
-# Use the existing zone's ID for records
-resource "aws_route53_record" "app" {
-  zone_id = data.aws_route53_zone.main.zone_id
-  name    = var.domain_name
-  type    = "A"
-
-  alias {
-    name                   = module.eks.cluster_endpoint
-    zone_id                = data.aws_route53_zone.main.zone_id
-    evaluate_target_health = true
-  }
-}
-
 # Create IAM role for ALB Ingress Controller
 resource "aws_iam_role" "alb_ingress_controller" {
   name = "eks-alb-ingress-controller"
