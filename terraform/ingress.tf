82fb1d46dd6c709235693372d0b2c37710ead75c
muzaparoff <muzyukins@gmail.com>
2025-05-20 16:31:20 +0300

    add: fixes cd

--- terraform/ingress.tf
+++ terraform/ingress.tf
@@ -1,8 +1,27 @@
 resource "kubernetes_ingress" "barkuni" {
+  metadata {
+    name      = "barkuni"
+    namespace = "default"
+  }
+
   spec {
     tls {
-      hosts      = ["your.domain.com"]
+      hosts       = ["your.domain.com"]
       secret_name = "barkuni-tls"
+    }
+
+    rule {
+      host = "your.domain.com"
+      http {
+        path {
+          path     = "/"
+          path_type = "Prefix"
+          backend {
+            service_name = "barkuni-service"
+            service_port = 5000
+          }
+        }
+      }
     }
   }
 }
